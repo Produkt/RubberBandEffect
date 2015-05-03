e0e23edbd588da8536445f7ca92beab4641aca53
Victor Baro <fs.baro@gmail.com>
2015-05-03 23:10:26 +0100

    Initial Commit

---
+++ FrictionCurves/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FrictionCurves
+//
+//  Created by Victor Baro on 3/5/15.
+//  Copyright (c) 2015 Produkt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
